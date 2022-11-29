c5dfe23c024d39a13547e701642b67bb50740b4b
Class Account <ee106a-aew@c105-8.local>
2022-11-29 14:38:43 -0800

    Get hard coded values for sawyer bot.

---
+++ project/src/move_sawyer/src/jacobian.py
@@ -0,0 +1,11 @@
+# use RViz to command Sawyer to 0 configuration
+# use RViz to read off q and w values, and thus find the xi's and the corresponding Jacobian matrix
+
+right_l0 = {"position": [0, 0, 0.08], "orientation": [0, 0, 0.003, 1]}
+right_l1 = {"position": [0.081, 0.05, 0.316], "orientation": [-0.5, 0.504, 0.499, 0.5]}
+right_l2 = {"position": [0.223, 0.191, 0.316], "orientation": [0.001, 0.709, -0.004, 0.705]}
+right_l3 = {"position": [0.483, 0.147, 0.315], "orientation": [-0.498, 0.505, 0.499, 0.499]}
+right_l4 = {"position": [0.607, 0.02, 0.315], "orientation": [0.002, 0.71, -0.002, 0.704]}
+right_l5 = {"position": [0.881, 0.05, 0.314], "orientation": [-0.5, 0.502, 0.498, 0.501]}
+right_l6 = {"position": [0.992, 0.154, 0.314], "orientation": [0.705, 0.058, 0.705, 0.06]}
+right_hand = {"position": [1.017, 0.154, 0.314], "orientation": [0.539, -0.457, 0.540, -0.456]}
