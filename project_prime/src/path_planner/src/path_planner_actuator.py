937406453c95cf73c7cb2984664b94d460a32b7b
sarinasabouri1 <55475329+sarinasabouri1@users.noreply.github.com>
2022-12-05 17:12:39 -0800

    New actuator implementation

--- project_prime/src/path_planner/src/path_planner_actuator.py
+++ project_prime/src/path_planner/src/path_planner_actuator.py
@@ -9,8 +9,13 @@ import sys
 from sawyer_forward_kinematics import get_position_from_fwd_kinematics
 import warnings
 
+import intera_interface
+
 def main():
     warnings.filterwarnings("error")
+
+   
+
     # Wait for the IK service to become available
     rospy.wait_for_service('compute_ik')
     rospy.init_node('service_query')
@@ -18,6 +23,10 @@ def main():
     compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
     while not rospy.is_shutdown():
         input('Press [ Enter ]: ')
+
+         # intera_interface speed
+        limb = intera_interface.Limb('right')
+        limb.set_joint_position_speed(0.3)
 
         # Construct the request
         request = GetPositionIKRequest()
@@ -31,8 +40,8 @@ def main():
         request.ik_request.pose_stamped.header.frame_id = "base"
         
         # Set the desired orientation for the end effector HERE
-        request.ik_request.pose_stamped.pose.position.x = 2.5
-        request.ik_request.pose_stamped.pose.position.y = 2.5
+        request.ik_request.pose_stamped.pose.position.x = 0.5
+        request.ik_request.pose_stamped.pose.position.y = 0.5
         request.ik_request.pose_stamped.pose.position.z = 0.5        
         request.ik_request.pose_stamped.pose.orientation.x = 0.0
         request.ik_request.pose_stamped.pose.orientation.y = 1.0
@@ -52,14 +61,11 @@ def main():
 
             # TRY THIS
             # Setting just the position without specifying the orientation
-            group.set_position_target([2.5, 2.5, 0.5])
+            group.set_position_target([0.5, 0.5, 0.5])
 
             # Plan IK
 
-            plan = get_best_plan(group)
-            user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
-
-
+            plan = get_best_plan_angles(group)
             
             
             # Execute IK if safe
@@ -69,59 +75,76 @@ def main():
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
 
-
-def compute_path_distance(positions):
-    total_distance = 0
-    for i in range(1,len(positions)):
-        start_position = positions[i-1]
-        end_position = positions[i]
-        total_distance += np.linalg.norm(end_position - start_position)
-    return total_distance
-
-
-def get_best_plan(group):
-    plans = []
-    best_plan = None
-    min_dist = float('inf')
+def get_best_plan_angles(group):
+    # plans = []
+    possible_solutions = []
     for i in range(50):
-        try:
-            plan = group.plan()
-        # checkpoint = input("check and enter to continue")
-        except Exception:
-            print("No path Found")
-            break
-
-
-        plans.append(plan)
+        plan = group.plan()
         joint_trajectory_angles = np.array([list(x.positions) for x in plan[1].joint_trajectory.points])
+        
+        change_in_theta = np.abs(np.array(joint_trajectory_angles[-1]) - np.array(joint_trajectory_angles[0]))
+        possible_solutions.append( (change_in_theta, np.max(change_in_theta)) )
 
-        positions = np.array([get_position_from_fwd_kinematics(angle) for angle in joint_trajectory_angles])
+    (best_solution, best_cost) = min(possible_solutions, key = lambda x: x[1])
+    print(best_solution)
+    print(best_cost)
+    return best_solution
 
-        distance = compute_path_distance(positions)
-        print(distance)
+# def compute_path_distance(positions):
+#     total_distance = 0
+#     for i in range(1,len(positions)):
+#         start_position = positions[i-1]
+#         end_position = positions[i]
+#         total_distance += np.linalg.norm(end_position - start_position)
+#     return total_distance
 
-        if distance < min_dist:
-            min_dist = distance
-            best_plan = plan
-    print("best plan distance: ", min_dist)
-    # attempting to speed up best plan
-    return best_plan
+# def get_best_plan_displacement(group):
+#     plans = []
+#     best_plan = None
+#     min_dist = float('inf')
+#     end_angles = []
+#     for i in range(50):
+#         try:
+#             plan = group.plan()
+#         # checkpoint = input("check and enter to continue")
+#         except Exception:
+#             print("No path Found")
+#             break
 
 
+#         plans.append(plan)
+#         joint_trajectory_angles = np.array([list(x.positions) for x in plan[1].joint_trajectory.points])
 
-def get_avg_joint_energy(all_joint_trajectory_positions):
-    joint_energys = []
-    for i in range(0,7):
-        joint_trajectory_positions = [x[i] for x in all_joint_trajectory_positions]
-        joint_energy = get_joint_energy(joint_trajectory_positions)
-        joint_energys.append(joint_energy)
-    average_energy = sum(joint_energys)/len(joint_energys)
-    return average_energy
+#         positions = np.array([get_position_from_fwd_kinematics(angle) for angle in joint_trajectory_angles])
+
+#         distance = compute_path_distance(positions)
+#         print(distance)
+
+#         end_angles.append(joint_trajectory_angles[len(joint_trajectory_angles)-1])
+
+#         if distance < min_dist:
+#             min_dist = distance
+#             best_plan = plan
+#     # print(end_angles)
+#     for angles in end_angles:
+#         print(angles)
+#     print("best plan distance: ", min_dist)
+#     # attempting to speed up best plan
+#     return best_plan
+
+# def get_avg_joint_energy(all_joint_trajectory_positions):
+#     joint_energys = []
+#     for i in range(0,7):
+#         joint_trajectory_positions = [x[i] for x in all_joint_trajectory_positions]
+#         joint_energy = get_joint_energy(joint_trajectory_positions)
+#         joint_energys.append(joint_energy)
+#     average_energy = sum(joint_energys)/len(joint_energys)
+#     return average_energy
 
 
-def get_joint_energy(joint_trajectory_positions):
-    energy = sum([x**2 for x in joint_trajectory_positions])
-    return energy
+# def get_joint_energy(joint_trajectory_positions):
+#     energy = sum([x**2 for x in joint_trajectory_positions])
+#     return energy
 
 
 # Python's syntax for a main() method
